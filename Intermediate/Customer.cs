a869b55dc2aff192779ff73d5bf1d866ecd80202
Carl Peters <31207893+cpjjpeters@users.noreply.github.com>
2018-07-15 21:16:05 +0200

    added a lot

---
+++ Intermediate/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Intermediate
+{
+    public class Customer
+    {
+        public String Name { get; set; }
+        public string Address { get; set; }
+        public Customer (string name, string address){ Name = name;
+            Address= address;
+        }
+
+        public override string ToString()
+        {
+            return Name + ", " + Address;
+        }
+    }
+}
---
+++ Intermediate/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Intermediate
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Make some Customer objects.
+            Customer[] customers =
+            {
+                new Customer("art", "100 Ash ave"),
+                new Customer("betty", "200 Beech blv"),
+                new Customer("art","300 Cedar street"),
+                new Customer("Chuck", "100 Ash ave"),
+            };
+
+            CustomerNameComparer comparer = new CustomerNameComparer();
+            List<Customer> list = new List<Customer>();
+            foreach (Customer cust in customers)
+                
+            {
+                if (!list.Contains(cust, comparer))
+                    list.Add(cust);
+            }
+            Dictionary<Customer, Customer> dict = new Dictionary<Customer, Customer>();
+            foreach (Customer cust in customers)
+            {
+                if (!dict.ContainsKey(cust))
+                    dict.Add(cust,cust);
+            }
+        }
+    }
+}
